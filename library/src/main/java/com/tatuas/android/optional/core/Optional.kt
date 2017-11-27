4282caa6ea75400d9f65224f24cdbe678cd64448
Tatsuya Sawai <a209050@gmail.com>
2017-11-27 16:00:41 +0900

    0.1.0-alpha1

---
+++ library/src/main/java/com/tatuas/android/optional/core/Optional.kt
@@ -0,0 +1,44 @@
+package com.tatuas.android.optional.core
+
+class Optional<out T> {
+
+    companion object {
+
+        private val EMPTY = Optional<Any>()
+
+        fun <T> ofNullable(value: T?) = if (value == null) empty() else of(value)
+
+        fun <T> of(value: T) = Optional(value)
+
+        fun empty() = EMPTY
+    }
+
+    val value: T?
+
+    private constructor() {
+        this.value = null
+    }
+
+    private constructor(value: T) {
+        this.value = value
+    }
+
+    fun isPresent() = value != null
+
+    fun <T> orElse(other: T) = value ?: other
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Optional<*>
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode() = value?.hashCode() ?: 0
+
+    override fun toString() = "Optional(value=$value)"
+}
---
+++ sample/src/main/java/com/tatuas/android/optionalcoresample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tatuas.android.optionalcoresample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.tatuas.android.optional.core.Optional
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "log"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txt1 = Optional.ofNullable(null)
+        val txt2 = Optional.of("hello")
+        val txt3 = Optional.empty()
+
+        Log.d(TAG, "txt1: " + txt1.orElse("").toString())
+        Log.d(TAG, "txt2: " + if (txt2.isPresent()) txt2.value else "empty")
+        Log.d(TAG, "txt3: " + txt3.value.toString())
+    }
+}
